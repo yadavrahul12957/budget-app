2733819dc098655671c87f12be1521fb07f35c75
Rahul <yadavrahul12957@gmail.com>
2019-06-11 03:33:49 +0530

    add btn event

--- app.js
+++ app.js
@@ -22,5 +22,21 @@ var UIController = (function() {
 
 var controller = (function(budgetCtrl, UICtrl) {
 
+    document.querySelector('.add__btn').addEventListener('click', function() {
+
+        // 1. Get the field input data
+
+        // 2. Add the items to the budget controller
+
+        // 3. Add the items to the UI
+
+        // 4. Calculate the budget.
+
+        // 5. Display the budget on UI.
+
+    });
+
+
+
 
 })(budgetController, UIController);
