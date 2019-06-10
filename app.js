5684193926ddddbc5d692492ce22c438ccfe234c
yadavrahul12957 <47967659+yadavrahul12957@users.noreply.github.com>
2019-06-11 03:02:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+//BUDGET CONTROLLER
+
+var budgetController = (function() {
+
+    //Some code
+
+})();
+
+
+//UI CONTROLLER
+
+var UIController = (function() {
+
+
+    //some code
+
+
+})();
+
+
+// GLOBAL APP CONTROLLER
+
+var controller = (function(budgetCtrl, UICtrl) {
+
+
+})(budgetController, UIController);
